5bffad8fb23a228ad806dbca0025f5050b110eda
Serafim <7620226@gmail.com>
2022-01-29 03:48:35 +0300

    feat: add buttons animation

--- script.js
+++ script.js
@@ -305,3 +305,32 @@ function getTranslation (currentLanguage) {
   })
 }
 
+//!--------------------------------- button animation
+
+const animatedButtons = document.querySelectorAll('.animated-button');
+
+for(let button of animatedButtons){
+button.addEventListener('click', animatedButtonHandler);
+}
+
+function animatedButtonHandler(e) {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const buttonTop = e.target.offsetTop;
+  const buttonLeft = e.target.offsetLeft;
+  
+  const xInside = x - buttonLeft;
+  const yInside = y - buttonTop + window.pageYOffset;
+
+  const circle = document.createElement('span');
+  circle.classList.add('circle');
+  circle.style.top = yInside + 'px';
+  circle.style.left = xInside + 'px';
+
+  this.appendChild(circle);
+  setTimeout(() => circle.remove(), 500);
+}
+
+
+
